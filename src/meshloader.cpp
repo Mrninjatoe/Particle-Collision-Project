ca7f826c1533b973a384862492c46a2b3750100a
Fippel <nils.kavemark@hotmail.com>
2018-04-20 06:10:00 +0200

    assimp stuff
    Signed-off-by: Fippel <nils.kavemark@hotmail.com>

--- src/meshloader.cpp
+++ src/meshloader.cpp
@@ -6,8 +6,7 @@
 
 
 MeshLoader::MeshLoader() {
-	const aiScene* asd = aiImportFile("bb8.FBX", aiProcessPreset_TargetRealtime_MaxQuality);
-	printf("%d", asd->mNumMeshes);
+	
 }
 
 MeshLoader::~MeshLoader() {
@@ -17,6 +16,46 @@ MeshLoader::~MeshLoader() {
 //Mesh& MeshLoader::loadMesh(const std::string& path) {
 //
 //}
+
+Mesh* MeshLoader::loadMesh(const char* path) {
+	const aiScene* test = aiImportFile(path, aiProcessPreset_TargetRealtime_MaxQuality);
+	//aiMesh* mesh = test->mMeshes[1];
+	//printf("%d", mesh);
+
+	aiMesh* mesh;
+	std::vector <Mesh::Vertex> vertices;
+	std::vector<unsigned short> indices;
+	for (int i = 0; i < test->mNumMeshes; i++) {
+		mesh = test->mMeshes[i];
+		for (int j = 0; j < mesh->mNumVertices; j++) {
+			Mesh::Vertex vertex;
+			glm::vec3 tmpVec;
+
+			tmpVec.x = mesh->mVertices[i].x;
+			tmpVec.y = mesh->mVertices[i].y;
+			tmpVec.z = mesh->mVertices[i].z;
+			vertex.pos = tmpVec;
+
+			tmpVec.x = mesh->mNormals[i].x;
+			tmpVec.y = mesh->mNormals[i].y;
+			tmpVec.z = mesh->mNormals[i].z;
+			vertex.normal = tmpVec;
+
+
+
+
+			vertices.push_back(vertex);
+		}
+
+		for (int j = 0; j < mesh->mNumFaces; j++) {
+			aiFace face = mesh->mFaces[j];
+			for (int k = 0; k < face.mNumIndices; k++) {
+				indices.push_back(face.mIndices[k]);
+			}
+		}
+	}
+	return new Mesh(vertices, indices);
+}
 
 Mesh* MeshLoader::getQuad() {
 	std::vector<Mesh::Vertex> vertices{
